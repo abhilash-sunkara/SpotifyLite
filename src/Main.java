09d680bc47dc1a4a82137c2b730267ec40fde8bc
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2022-08-29 18:28:41 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("SpotifyLite");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
